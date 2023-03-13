0b1433f2a31d890a6df0fe4c4ff4a559df9f6902
anmuhina <112845722+anmuhina@users.noreply.github.com>
2023-03-13 19:40:27 +0300

    Update index.php

--- files/index.php
+++ files/index.php
@@ -73,7 +73,8 @@ if ($errors) {
 
 $user = 'u52811';
 $pass = '8150350';
-$db = new PDO('mysql:host=localhost;dbname=u52811', $user, $pass, [PDO::ATTR_PERSISTENT => true]);
+$db = new PDO('mysql:host=localhost;dbname=u52811', $user, $pass,
+  [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); 
 
 // Подготовленный запрос. Не именованные метки.
 
