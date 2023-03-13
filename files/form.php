8df4795a3da38add3c9d3fc5181d50d061c59457
anmuhina <112845722+anmuhina@users.noreply.github.com>
2023-03-13 17:03:21 +0300

    Update form.php

--- files/form.php
+++ files/form.php
@@ -1,7 +1,7 @@
- <form action="http://u52811.kubsu-dev.ru/backend3/files/index.php" class="forma" method="POST">
+ <form action="" class="forma" method="POST">
     <label><br>
         Имя:<br>
-        <input id="data" name="fio" placeholder="Введите Ваше имя">
+        <input id="data" name="name" placeholder="Введите Ваше имя">
     </label><br>
     <label>
         Email:<br>
@@ -10,7 +10,7 @@
     <label>
         Дата рождения:<br>
         
-         <select name="year">
+         <select name="date_of_birth">
          <?php 
           for ($i = 1922; $i <= 2022; $i++) {
           printf('<option value="%d">%d год</option>', $i, $i);
@@ -23,12 +23,12 @@
       <label><input id="data" type="radio" checked="checked" name="sex">ж</label>
       <label><input id="data" type="radio" name="sex">м</label><br />
     Количество конечностей:<br />
-      <label><input id="data" type="radio" checked="checked" name="limb"> 2 </label>
-      <label><input id="data" type="radio" name="limb"> 3 </label>
-      <label><input id="data" type="radio" name="limb"> 4 </label><br>
+      <label><input id="data" type="radio" checked="checked" name="amount_of_limbs"> 2 </label>
+      <label><input id="data" type="radio" name="amount_of_limbs"> 3 </label>
+      <label><input id="data" type="radio" name="amount_of_limbs"> 4 </label><br>
     <label>
         Сверхспособности:<br>
-        <select id="data" name="superpowers[]" multiple="multiple">
+        <select id="data" name="abilities[]" multiple="multiple">
           <option value="Значение1">Бессмертие</option>
           <option value="Значение2">Прохождение сквозь стены</option>
           <option value="Значение3">Левитация</option>
@@ -38,7 +38,7 @@
         Биография:<br />
         <textarea id="data" name="biography" placeholder="Введите текст"></textarea>
     </label><br>
-    <label><input id="data" type="checkbox" checked="checked" name="check">С контрактом ознакомлен(а)</label><br>
+    <label><input id="data" type="checkbox" checked="checked" name="informed">С контрактом ознакомлен(а)</label><br>
     <input id="data" type="submit" value="Отправить">
   </form>
     
