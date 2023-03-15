b397c21cb22324056fe69eff03c8e8dd65a6e329
anmuhina <112845722+anmuhina@users.noreply.github.com>
2023-03-15 18:09:02 +0300

    Update index.php

--- files/index.php
+++ files/index.php
@@ -99,7 +99,7 @@ catch(PDOException $e) {
 
 $app_id = $db->lastInsertId();
 
-$sql_abilities = "INSERT INTO 'abilities' ('id', 'name_of_ability') VALUES (10, 'Бессмертие'), (20, 'Прохождение сквозь стены'), (30, 'Левитация')";
+$sql_abilities = "INSERT INTO abilities (id, name_of_ability) VALUES (10, 'Бессмертие'), (20, 'Прохождение сквозь стены'), (30, 'Левитация')";
 $db->query($sql_abilities);
 
 
