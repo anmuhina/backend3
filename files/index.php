2107877f73dc777d2f8cc7bdcf6b8e8c9543894e
anmuhina <112845722+anmuhina@users.noreply.github.com>
2023-03-19 18:15:05 +0300

    Update index.php

--- files/index.php
+++ files/index.php
@@ -3,7 +3,10 @@ header('Content-Type: text/html; charset=UTF-8');
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
   if (!empty($_GET['save'])) {
-    print('Спасибо, результаты сохранены.');
+    <script type="text/javscript">
+       alert("Спасибо, результаты сохранены.");
+    </script>
+    //print('Спасибо, результаты сохранены.');
   }
   include('form.php');
   exit();
