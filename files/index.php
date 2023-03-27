614b78b4d0d3844ab593e7028b74e1c3c7d70244
anmuhina <112845722+anmuhina@users.noreply.github.com>
2023-03-27 08:45:05 +0300

    Update index.php

--- files/index.php
+++ files/index.php
@@ -10,7 +10,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 }
 
 $errors = FALSE;
-if (empty($_POST['name'])) {
+if (empty($_POST['name']) || !preg_match('/^\s*[a-zA-Zа-яА-Я]+\s*$/', $_POST['name'])) {
   print('Введите имя.<br/>');
   $errors = TRUE;
 }
